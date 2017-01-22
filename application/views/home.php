2fa883883201e61fb56325c4ebf44ed7f18eb8b4
NicholasThrom <narthrom@edu.uwaterloo.ca>
2017-01-22 08:59:57 -0500

    Made mobile friendly.

--- application/views/home.php
+++ application/views/home.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html>
 <head>
     <title>Endless Pathway</title>
+    <meta name="viewport" content="width=device-width">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url(); ?>assets/images/favicon.ico" />
