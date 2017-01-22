d859e62987b8f6ba7e92d9234484c6b746afe926
NicholasThrom <narthrom@edu.uwaterloo.ca>
2017-01-21 20:47:45 -0500

    Fixed display in a number of ways, externalized the js and css.

--- application/views/home.php
+++ application/views/home.php
@@ -6,170 +6,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
     <title>Endless Pathway</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <style>
-    body {
-        background-color: #36414D;
-        color: #B3D9FF;
-        font-family: "Lucida Console", Monaco, monospace;
-        font-weight: 100;
-        text-align: center;
-    }
-
-    .situation {
-        width: 30vw;
-        margin-left: auto;
-        margin-right: auto;
-        text-align: center;
-        margin-top: 0.3rem;
-        border: 1px solid #B3D9FF;
-        padding: 1rem;
-        border-radius: 1rem;
-    }
-
-    .option {
-        width: 20vw;
-        border: 1px solid #B3D9FF;
-        padding: 0.5rem;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 1rem;
-        cursor: pointer;
-    }
-
-    .option a {
-        text-decoration: none;
-        color: inherit;
-        display: inline-block;
-        width: 100%;
-        text-align: center;
-    }
-
-    .option.s1:hover {
-        background-color: #45364D;
-        color: #CFA1E6;
-        border-color: #CFA1E6;
-    }
-
-    .option.s2:hover {
-        background-color: #364D3E;
-        color: #A1E6B8;
-        border-color: #A1E6B8;
-    }
-
-    .option.s3:hover {
-        background-color: #4D4736;
-        color: #E6D4A1;
-        border-color: #E6D4A1;
-    }
-
-    .input {
-        background-color: transparent;
-        outline: none;
-        border: none;
-        border-bottom: 1px solid #B3D9FF;
-        width: 20vw;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 1.5rem;
-        color: inherit;
-        font-family: inherit;
-        font-size: inherit;
-        text-align: center;
-        padding: 0.3rem;
-        resize: none;
-    }
-
-    #action-input {
-        display: block;
-        margin-top: 3rem;
-    }
-
-    #description-input {
-        display: none;
-        border-radius: 1rem;
-        border: 1px solid #B3D9FF;
-        opacity: 0;
-        resize: none;
-        padding-top: 1.2rem;
-        padding-bottom: 0.5rem;
-        vertical-align:middle;
-    }
-
-    #submit-new {
-        display: none;
-        margin-left: auto;
-        margin-right: auto;
-        cursor: pointer;
-        margin-top: 1.5rem;
-        opacity: 0;
-    }
-
-    #submit-new:hover {
-        font-weight: 900;
-    }
-
-    #report {
-        cursor: pointer;
-        width: 50%;
-        display: inline-block;
-        float: right;
-        text-align: right;
-    }
-
-    #report:hover {
-        font-weight: 900;
-    }
-
-    #restart{
-        cursor: pointer;
-        width: 50%;
-        display: inline-block;
-        float: left;
-        text-align: left;
-    }
-
-    #restart:hover {
-        font-weight: 900;
-    }
-
-    #buttons {
-        margin-left: auto;
-        margin-right: auto;
-        width: 30vw;
-        height: 1rem;
-        margin-top: 10vh;
-    }
-
-    .warning {
-        color: #E65C5C;
-        font-size: 0.8rem;
-        margin-top: 0.3rem;
-        display: none;
-    }
-
-    ::-webkit-input-placeholder {
-        color: #6B8199;
-    }
-
-    :-moz-placeholder {
-        color: #6B8199;
-    }
-
-    ::-moz-placeholder {
-        color: #6B8199;
-    }
-
-    :-ms-input-placeholder {
-        color: #6B8199;
-    }
-    </style>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/main.css">
 </head>
 <body>
     <div id = "buttons">
         <?php if ($node['reports'] >= 0) { ?>
             <div id = "report">Report</div>
         <?php } ?>
-        
+
         <div id = "restart">Restart</div>
     </div>
 
@@ -178,8 +22,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
 
     <?php $i = 0; foreach($options as $option) { $i++; ?>
-        <div class = "option s<?php echo $i; ?>">
-            <a href = "<?php echo base_url(); ?>?i=<?php echo $option['id_node']?>"><?php echo $option['action']; ?></a>
+        <div onclick = "goToPage(<?php echo $option['id_node']?>);" class = "option s<?php echo $i; ?>">
+            <?php echo $option['action']; ?>
         </div>
     <?php } ?>
 
@@ -191,81 +35,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <div id = "submit-new">Submit</div>
 </body>
-<script>
-
-    var goToPage = function (id)
-    {
-        window.location = "<?php echo base_url(); ?>?i=" + id;
-    };
-
-    var submit = function ()
-    {
-        if ($("#action-input").val().trim() != "" && $("#description-input").val().trim() != "")
-        {
-            $.ajax({
-                url: "<?php echo base_url(); ?>index.php/main/addNode",
-                type: "post",
-                data: {
-                    a: $("#action-input").val(),
-                    d: $("#description-input").val(),
-                    i: "<?php echo $id_node; ?>"
-                },
-                success: function (data) {
-                    if (data != -1)
-                    {
-                        goToPage(data);
-                    }
-                    else
-                    {
-                        $("#action-repeat-warning").css("display", "block");
-                        $("#action-repeat-warning").html("The action \"" + $("#action-input").val() + "\" is already taken.");
-                    }
-                }
-            });
-        }
-    };
-
-    var report = function ()
-    {
-        $.ajax({
-            url:"<?php echo base_url(); ?>index.php/main/fileReport",
-            type: "post",
-            data: {
-                i: "<?php echo $id_node; ?>"
-            },
-            success: function () {
-                $("#report").css("color", "#E65C5C");
-                $("*").click(function () {});
-                $("*").attr("href", "");
-                setTimeout(function () {goToPage(1);}, 1000);
-            }
-        });
-    };
-
-    var restart = function ()
-    {
-        goToPage(1);
-    };
-
-    $("#action-input").focus(function () {
-        $("#description-input").css("display", "block");
-        $("#description-input").animate({opacity: 1});
-        $("#submit-new").css("display", "inline-block");
-        $("#submit-new").animate({opacity: 1});
-    });
-
-    $("#submit-new").click(submit);
-    $("#report").click(report);
-    $("#restart").click(restart);
-
-    $(document).keydown(function (event)
-    {
-        switch (event.keyCode)
-        {
-            case 13:
-                submit();
-                break;
-        }
-    });
-</script>
+<script src = "<?php echo base_url(); ?>assets/js/main.js"></script>
 </html>
